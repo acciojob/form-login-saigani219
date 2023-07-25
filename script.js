fb72c459246184f35a453e8d169cd33712af22a6
Sai Ganesh <105867763+saigani219@users.noreply.github.com>
2023-07-25 21:55:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let firstName = document.getElementsByTagName("input")[0].value;
+	let lastName = document.getElementsByTagName("input")[1].value;
+	alert(firstName + " " + lastName);
 }
